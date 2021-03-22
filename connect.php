9d7f84e3311723243bee0884f63fb17f38649890
mrplow <1490165+mrplow@users.noreply.github.com>
2021-03-21 17:06:01 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,7 @@
 <?php
-$hostname = "mariaDB";
-$username = "databaseuser";
-$password = "changeme";
-$db = "podcast_clips";
+$GLOBALS["mysql_hostname"] = "podcast_clips_mariaDB";
+$GLOBALS["mysql_username"] = "databaseuser";
+$GLOBALS["mysql_password"] = "changeme";
+$GLOBALS["mysql_database"] = "podcast_clips";
+
 ?>
